520839d0ca74d73b99966e3c4b3845037434c31a
Andreas Muller <hahakid@gmail.com>
2012-12-29 16:43:59 +0100

    Handled "pong" messages and compute time offset.

--- dev_MultiScreenCommonTiming/src/OSCManager.cpp
+++ dev_MultiScreenCommonTiming/src/OSCManager.cpp
@@ -27,8 +27,8 @@ void OSCManager::init( int _uniqueComputerID )
 {
 	uniqueComputerID = _uniqueComputerID;
 	
-	sender.setup( "localhost", 23456 );
-	//receiver;
+	sender.setup( "192.168.3.2", 7777 );
+	receiver.setup( 7778 );
 	
 	commonTimeOsc.init( &sender, uniqueComputerID );
 	
@@ -46,7 +46,21 @@ void OSCManager::_update(ofEventArgs &e)
 		ofxOscMessage m;
 		receiver.getNextMessage(&m);
 		
-		//m.getAddress()
+		if( m.getAddress() == "/pong" )
+		{
+			//cout << "Received Pong, ID: " << m.getArgAsInt32(0) << "  serverTime: " << m.getArgAsInt32(1) << "  origTimeStamp: " << m.getArgAsInt32(2) << endl;
+			
+			// pong messages come in as: /pong, receiving computer ID (int), server timestamp (int), timestamp when remote computer sent (int)
+			
+			// if the message is for us
+			if( m.getArgAsInt32(0) == uniqueComputerID )
+			{
+				int serverTime = m.getArgAsInt32(1);
+				int originalTimeStamp = m.getArgAsInt32(2);
+				
+				commonTimeOsc.newReading( serverTime, commonTimeOsc.getInternalTimeMillis() - originalTimeStamp );
+			}
+		}
 	}
 	
 }
