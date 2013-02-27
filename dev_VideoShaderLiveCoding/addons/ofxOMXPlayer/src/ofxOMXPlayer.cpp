8bbe80d4e47b93904c02425c1bdf9b9f6cdd564c
root <root@raspberrypi.(none)>
2013-02-27 13:22:30 +0000

    Adding the shader livecoding over video sample, this will need a re-write once ofxOMXPlayer has stabilized.

---
+++ dev_VideoShaderLiveCoding/addons/ofxOMXPlayer/src/ofxOMXPlayer.cpp
@@ -0,0 +1,283 @@
+/*
+ *  ofxOMXPlayer.cpp
+ *  openFrameworksLib
+ *
+ *  Created by jason van cleave on 2/15/13.
+ *  Copyright 2013 jasonvancleave.com. All rights reserved.
+ *
+ */
+
+#include "ofxOMXPlayer.h"
+
+ofxOMXPlayer::ofxOMXPlayer()
+{
+	isReady = false;
+	videoWidth = 0;
+	videoHeight =0;
+	isMPEG               = false;
+	hasVideo           = false;
+	packet = NULL;
+	moviePath = "moviePath is undefined";
+	clock = NULL;
+	playerTex = NULL;
+	pixels = NULL;
+	m_Pause = false;
+	duration = 0.0;
+	
+}
+void ofxOMXPlayer::loadMovie(string filepath)
+{
+	moviePath = filepath; 
+	rbp.Initialize();
+	omxCore.Initialize();
+	
+	clock = new OMXClock(); 
+	bool doDumpFormat = false;
+
+	if(omxReader.Open(moviePath.c_str(), doDumpFormat))
+	{
+		
+		ofLogVerbose() << "omxReader successfully opened moviePath : " << moviePath;
+		hasVideo     = omxReader.VideoStreamCount();
+		if (hasVideo) 
+		{
+			bool hasAudio = false; //not implemented yet
+			if(clock->OMXInitialize(hasVideo, hasAudio))
+			{
+				ofLogVerbose() << "clock Init success";
+				
+				omxReader.GetHints(OMXSTREAM_VIDEO, streamInfo);
+				videoWidth	= streamInfo.width;
+				videoHeight	= streamInfo.height;
+
+				generateEGLImage();
+				bool didOpenVideo = omxPlayerVideo.Open(streamInfo, clock, eglImage);
+				if(streamInfo.nb_frames>0 && omxPlayerVideo.GetFPS()>0)
+				{
+					duration = streamInfo.nb_frames / omxPlayerVideo.GetFPS();
+					ofLogVerbose() << "duration: " << duration;
+				}
+				
+				
+
+				if (didOpenVideo) 
+				{
+					ofLogVerbose() << "Opened video!";
+					isReady  = true;
+					
+				}else 
+				{
+					ofLogError() << "could not open video";
+				}
+				
+			}else 
+			{
+				ofLogError() << "clock could not init";
+			}
+		}else 
+		{
+			ofLogError() << "No Video detected";
+		}
+	}else 
+	{
+		ofLogError() << "omxReader could not open file: " << moviePath;
+	}
+	
+}
+void ofxOMXPlayer::generateEGLImage()
+{
+	ofDisableArbTex();
+	
+	ofAppEGLWindow *appEGLWindow = (ofAppEGLWindow *) ofGetWindowPtr();
+	display = appEGLWindow->getEglDisplay();
+	context = appEGLWindow->getEglContext();
+	ofLogVerbose() << "videoWidth: " << videoWidth;
+	ofLogVerbose() << "videoHeight: " << videoHeight;
+	
+	tex.allocate(videoWidth, videoHeight, GL_RGBA);
+	tex.getTextureData().bFlipTexture = true;
+	tex.setTextureWrap(GL_REPEAT, GL_REPEAT);
+	//textureSource.setTextureMinMagFilter(GL_NEAREST, GL_NEAREST);
+	//textureSource.setTextureWrap(GL_CLAMP_TO_EDGE, GL_CLAMP_TO_EDGE);
+	textureID = tex.getTextureData().textureID;
+	
+	
+	glEnable(GL_TEXTURE_2D);
+	
+	
+	// setup first texture
+	int dataSize = videoWidth * videoHeight * 4;
+	
+	GLubyte* pixelData = new GLubyte [dataSize];
+	
+	
+    memset(pixelData, 0xff, dataSize);  // white texture, opaque
+	
+	glBindTexture(GL_TEXTURE_2D, textureID);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, videoWidth, videoHeight, 0,
+				 GL_RGBA, GL_UNSIGNED_BYTE, pixelData);
+	
+	delete[] pixelData;
+	
+	
+	// Create EGL Image
+	eglImage = eglCreateImageKHR(
+								 display,
+								 context,
+								 EGL_GL_TEXTURE_2D_KHR,
+								 (EGLClientBuffer)textureID,
+								 0);
+    glDisable(GL_TEXTURE_2D);
+	if (eglImage == EGL_NO_IMAGE_KHR)
+	{
+		ofLogError()	<< "Create EGLImage FAIL";
+		return;
+	}
+	else
+	{
+		ofLogVerbose()	<< "Create EGLImage PASS";
+	}
+	pixels = new ofPixels();
+	pixels->allocate(videoWidth, videoHeight, OF_PIXELS_RGBA);
+}
+
+//---------------------------------------------------------------------------
+//for getting a reference to the texture
+ofTexture & ofxOMXPlayer::getTextureReference(){
+	if(playerTex == NULL){
+		return tex;
+	}
+	else{
+		return *playerTex;
+	}
+}
+
+void ofxOMXPlayer::update()
+{
+	if(!isReady)
+	{
+		return;
+	}
+	
+	if(!packet)
+	{
+		packet = omxReader.Read();
+	}
+	
+	if(packet && omxReader.IsActive(OMXSTREAM_VIDEO, packet->stream_index))
+    {
+		if(omxPlayerVideo.AddPacket(packet))
+		{
+			packet = NULL;
+		}else 
+		{
+			OMXClock::OMXSleep(10);
+		}
+	}else 
+	{
+		if(packet)
+		{
+			omxReader.FreePacket(packet);
+			packet = NULL;
+		}
+	}
+}
+//--------------------------------------------------------
+void ofxOMXPlayer::play(){
+	
+}
+
+//--------------------------------------------------------
+void ofxOMXPlayer::stop(){
+	
+}
+
+//---------------------------------------------------------------------------
+void ofxOMXPlayer::setPaused(bool _bPause){
+	m_Pause = _bPause;
+	if(m_Pause)
+	{
+		omxPlayerVideo.SetSpeed(OMX_PLAYSPEED_PAUSE);
+		clock->OMXPause();
+	}else 
+	{
+		omxPlayerVideo.SetSpeed(OMX_PLAYSPEED_NORMAL);
+		clock->OMXResume();
+	}
+
+			
+}
+
+//---------------------------------------------------------------------------
+unsigned char * ofxOMXPlayer::getPixels(){
+	if( pixels != NULL ){
+		return pixels->getPixels();
+	}
+	return NULL;	
+}
+
+//---------------------------------------------------------------------------
+ofPixelsRef ofxOMXPlayer::getPixelsRef(){
+	if (pixels ==NULL) 
+	{
+		//TODO figure this out
+		ofLogError() << "probably going to crash";
+	}
+	return *pixels;
+}
+
+//------------------------------------
+void ofxOMXPlayer::draw(float _x, float _y, float _w, float _h)
+{
+	if(!isReady) return;
+	getTextureReference().draw(_x, _y, _w, _h);	
+}
+
+//------------------------------------
+void ofxOMXPlayer::draw(float _x, float _y)
+{
+	if(!isReady) return;
+	getTextureReference().draw(_x, _y);
+}
+
+//---------------------------------------------------------------------------
+float ofxOMXPlayer::getDuration(){
+	
+	
+	return duration;
+}
+//----------------------------------------------------------
+float ofxOMXPlayer::getWidth()
+{
+	return videoWidth;
+}
+
+//----------------------------------------------------------
+float ofxOMXPlayer::getHeight()
+{
+	return videoHeight;
+}
+
+void ofxOMXPlayer::close()
+{
+	if(isReady) 
+	 {
+		 clock->OMXStop();
+		 clock->OMXStateIdle();
+		 omxPlayerVideo.Close();
+	 }
+	 if(packet)
+	 {
+		 omxReader.FreePacket(packet);
+		 packet = NULL;
+	 }	
+		omxReader.Close();
+
+	 if (eglImage !=NULL) 
+	 {
+		 eglDestroyImageKHR(display, eglImage);
+	 }
+
+	omxCore.Deinitialize();
+	rbp.Deinitialize();
+}
---
+++ dev_VideoShaderLiveCoding/addons/ofxOMXPlayer/src/ofxOMXPlayer.h
@@ -0,0 +1,108 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofAppEGLWindow.h"
+
+extern "C" 
+{
+	#include <libavformat/avformat.h>
+	#include <libavutil/avutil.h>
+};
+
+#include "RBP.h"
+#include "OMXClock.h"
+#include "OMXPlayerVideo.h"
+
+
+class ofxOMXPlayer 
+{
+public:
+	ofxOMXPlayer();
+	void loadMovie(string filepath);
+	void update();
+	
+	void 				play();
+	void 				stop();
+	
+	//bool 				isFrameNew();
+	unsigned char * 	getPixels();
+	ofPixelsRef			getPixelsRef();
+//	float 				getPosition();
+//	float 				getSpeed();
+	float				duration;
+	float 				getDuration();
+//	bool				getIsMovieDone();
+	
+//	void 				setPosition(float pct);
+//	void 				setVolume(float volume); // 0..1
+//	void 				setLoopState(ofLoopType state);
+//	ofLoopType			getLoopState();
+//	void   				setSpeed(float speed);
+//	void				setFrame(int frame);  // frame 0 = first frame...
+	
+	ofTexture &			getTextureReference();
+	void 				draw(float x, float y, float w, float h);
+	void 				draw(float x, float y);
+
+	
+	void				setAnchorPercent(float xPct, float yPct);	//set the anchor as a percentage of the image width/height ( 0.0-1.0 range )
+	void				setAnchorPoint(float x, float y);				//set the anchor point in pixels
+	void				resetAnchor();								//resets the anchor to (0, 0)
+	
+	void 				setPaused(bool bPause);
+	
+	int					getCurrentFrame();
+	int					getTotalNumFrames();
+	
+//	void				firstFrame();
+//	void				nextFrame();
+//	void				previousFrame();
+	
+	float 				getHeight();
+	float 				getWidth();
+	
+//	bool				isPaused();
+//	bool				isLoaded();
+//	bool				isPlaying();
+	
+	CRBP                  rbp;
+	COMXCore              omxCore;
+	OMXClock * clock;
+	
+	OMXPlayerVideo    omxPlayerVideo;
+	OMXReader         omxReader;
+	
+	COMXStreamInfo    streamInfo;
+	
+	bool isMPEG;
+	bool hasVideo;
+	
+	DllBcmHost        bcmHost;
+	
+	bool isReady;
+	
+	OMXPacket* packet;
+	
+	GLuint textureID;
+	bool m_Pause;
+	
+	
+	int videoWidth;
+	int videoHeight;
+	
+	EGLDisplay display;
+	EGLContext context;
+	EGLImageKHR eglImage;
+	
+	void generateEGLImage();
+	ofPixels* pixels;
+	void close();
+private:
+	
+	ofTexture tex;
+	ofTexture * playerTex; // a seperate texture that may be optionally implemented by the player to avoid excessive pixel copying.
+	ofPixelFormat internalPixelFormat;
+	string moviePath;
+	
+};
+
