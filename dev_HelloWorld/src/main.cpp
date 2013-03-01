5bb0b2364e9da54a07b43f9998f158ba34fa05aa
Andreas Muller <hahakid@gmail.com>
2013-03-01 16:34:18 +0000

    Forgot to add the right main.cpp

--- dev_HelloWorld/src/main.cpp
+++ dev_HelloWorld/src/main.cpp
@@ -1,10 +1,9 @@
+#include "ofMain.h"
 #include "testApp.h"
-#include "ofAppGlutWindow.h"
 
-//--------------------------------------------------------------
-int main(){
-	ofAppGlutWindow window; // create a window
-	// set width, height, mode (OF_WINDOW or OF_FULLSCREEN)
-	ofSetupOpenGL(&window, 1024, 768, OF_WINDOW);
-	ofRunApp(new testApp()); // start the app
+//========================================================================
+int main( )
+{
+	ofSetupOpenGL(1280, 720, OF_WINDOW); // OF_WINDOW or OF_FULLSCREEN
+	ofRunApp( new testApp());
 }
