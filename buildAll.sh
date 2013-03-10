7b652ec5acf0272beb90715903f8691adc68f92b
Jason Van Cleave <jvcleave@gmail.com>
2013-03-10 06:16:19 -0400

    adding buildAll script

---
+++ buildAll.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export LC_ALL=C
+#export DISTCC_FALLBACK=0
+
+
+for example in $( ls . )
+do
+	if [ -d "${example}" ]; then
+		cd $example
+		echo -e "\nBUILDING " $example " \n"
+		#make clean
+		make
+		ret=$?
+		if [ $ret -ne 0 ]; 
+		then
+			echo -e "\n" $example " compile FAILED \n" 
+		else
+			echo -e "\n" $example "compile SUCCESS \n" 
+		fi
+		cd ../
+	fi
+done
