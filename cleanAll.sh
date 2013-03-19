1b1b4e50d3047efa2e6b14fc214fa0faafd5223c
Jason Van Cleave <jvcleave@gmail.com>
2013-03-19 15:58:36 -0400

    removed dupes

---
+++ cleanAll.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export LC_ALL=C
+#export DISTCC_FALLBACK=0
+
+
+for example in $( ls . )
+do
+	if [ -d "${example}" ]; then
+		cd $example
+		echo -e "\nCLEANING " $example " \n"
+		make clean
+		ret=$?
+		if [ $ret -ne 0 ]; 
+		then
+			echo -e "\n" $example " clean FAILED \n" 
+		else
+			echo -e "\n" $example "clean SUCCESS \n" 
+		fi
+		cd ../
+	fi
+done
