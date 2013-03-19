c0c61492d22f83dea9e5e2ad62b5936a776e0db9
Jason Van Cleave <jvcleave@gmail.com>
2013-03-19 16:00:19 -0400

    removed deleted

--- dev_HelloWorld/src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "ofMain.h"
-#include "testApp.h"
-
-//========================================================================
-int main( )
-{
-	ofSetupOpenGL(1280, 720, OF_WINDOW); // OF_WINDOW or OF_FULLSCREEN
-	ofRunApp( new testApp());
-}
